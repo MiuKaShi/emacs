b89ba14543a5aeb10db1ed5722114aa04887caa6
miuka <d.lolimiuku@gmail.com>
2025-03-28 17:53:41 +0900

    update .gitignore

--- custom.el
+++ custom.el
@@ -10,8 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-super-agenda marginalia embark-consult consult embark orderless vertico diredfl pyvenv haskell-mode lua-mode bazel tuareg cargo rust-mode cmake-font-lock cmake-mode bison-mode rmsbolt graphviz-dot-mode yaml-mode devdocs citre dumb-jump flycheck projectile quickrun spdx diff-hl magit lsp-ui lsp-mode company evil-collection evil-surround evil atomic-chrome fanyi webpaste separedit gcmh avy rg hide-mode-line centered-cursor-mode hl-prog-extra which-key diminish rime neotree dashboard rainbow-mode shackle doom-modeline doom-themes delight try quelpa-use-package quelpa no-littering)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
